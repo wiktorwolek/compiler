50a4c708181e16b1723b840e2474345ff168d64c
Małgorzata Sokół <malgorzata@e-sokol.pl>
2024-05-15 11:00:16 +0200

    Dodane wczytywanie nazwy pliku z argumentu

--- compiler.py
+++ compiler.py
@@ -7,26 +7,34 @@ from LLVMActions import LLVMActions
 from LLVMGenerator import LLVMGenerator
 
 def main(args):
-    # Step 1: Load input source into the stream object
-    stream = FileStream("test.x")
 
-    # Step 2: Create an instance of AssignmentStLexer
-    lexer = ExprLexer(stream)
+    if len(args) > 1:
+        print(args)
+        # Step 1: Load input source into the stream object
+        stream = FileStream(args[1])
 
-    # Step 3: Convert the input source into a list of tokens
-    tokens = CommonTokenStream(lexer)
+        # Step 2: Create an instance of AssignmentStLexer
+        lexer = ExprLexer(stream)
 
-    # Step 4: Create an instance of the AssignmentStParser
-    parser = ExprParser(tokens)
+        # Step 3: Convert the input source into a list of tokens
+        tokens = CommonTokenStream(lexer)
 
-    # Step 5: Create parse tree
-    tree = parser.prog()
-    print(tree.toStringTree(recog=parser))
-    walker = ParseTreeWalker()
-    try:
-        walker.walk(LLVMActions(), tree)
-    except:
-         print(LLVMGenerator().generate())
-         print(":(")
+        # Step 4: Create an instance of the AssignmentStParser
+        parser = ExprParser(tokens)
+
+        # Step 5: Create parse tree
+        tree = parser.prog()
+        print(tree.toStringTree(recog=parser))
+        walker = ParseTreeWalker()
+        try:
+            walker.walk(LLVMActions(), tree)
+        except:
+            print(LLVMGenerator().generate())
+            print(":(")
+
+    else:
+        print("Not enough arguments")
+
+
 if __name__ == '__main__':
     main(sys.argv)
